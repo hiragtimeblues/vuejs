edbe23c6acab4231a7704071d1a3ddf0031105cb
Nozomu Hirata <guns1of1brixton@gmail.com>
2019-05-12 11:49:00 +0900

    190512_修正

--- bitcoin_price/js/main.js
+++ bitcoin_price/js/main.js
@@ -1,3 +1,12 @@
-var app = new VTTCue({
+var app = new Vue({
     el: '#app',
+    data: {
+        bpi: null
+    },
+    mounted: function() {
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function (response) {
+            // console.log(response.data.bpi);
+            this.bpi = response.data.bpi
+        }, bind(this))
+    }
 })
